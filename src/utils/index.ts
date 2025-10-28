bc4695c09bdf39bcbc6681f5b94dcdc0f4c11c63
farhad0724 <162090888+farhad0724@users.noreply.github.com>
2025-10-29 01:00:39 +0500

    Add helper to get milliseconds until midnight to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,3 +10,11 @@ export const formatDateTime = (isoString: string) => {
     minute: '2-digit'
   })
 }
+
+export const getMsUntilMidnight = () => {
+  const now = new Date()
+  const tomorrow = new Date(now)
+  tomorrow.setDate(tomorrow.getDate() + 1)
+  tomorrow.setHours(0, 0, 0, 0)
+  return tomorrow.getTime() - now.getTime()
+}
