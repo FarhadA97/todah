59829c05ef4d1a091c69c6d4ae9ca563eb993592
farhad0724 <162090888+farhad0724@users.noreply.github.com>
2025-10-29 01:00:11 +0500

    Add format date time helper to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,12 @@
 export const getTodayString = () => new Date().toLocaleDateString('en-CA')
+
+export const formatDateTime = (isoString: string) => {
+  const date = new Date(isoString)
+  return date.toLocaleString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit'
+  })
+}
